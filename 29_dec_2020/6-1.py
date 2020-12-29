a93e7dfe142be1f2f96b1ca42f73d9daf4794c86
ironmanvim <vishalchiluveri@gmail.com>
2020-12-30 01:03:23 +0530

    updates - renamed folder from problem number to date - added 29_dec_2020 problem

---
+++ 29_dec_2020/6-1.py
@@ -0,0 +1,31 @@
+def in_IP_range(number):
+    return number >= 0 and number <= 255
+
+
+def number_with_leading_zeros(number):
+    return str(int(number)) != number
+
+
+number = str(127001)
+ptr1, ptr2, ptr3 = 1, 2, 3
+
+while True:
+    if not in_IP_range(int(number[:ptr1])) or number_with_leading_zeros(number[:ptr1]):
+        break
+    elif not in_IP_range(int(number[ptr1:ptr2])) or number_with_leading_zeros(number[ptr1:ptr2]):
+        ptr1 += 1
+    elif not in_IP_range(int(number[ptr2:ptr3])) or number_with_leading_zeros(number[ptr2:ptr3]):
+        ptr2 += 1
+    elif not in_IP_range(int(number[ptr3:])) or number_with_leading_zeros(number[ptr3:]):
+        ptr3 += 1
+    else:
+        print('{}.{}.{}.{}'.format(
+            number[0:ptr1], number[ptr1:ptr2], number[ptr2:ptr3], number[ptr3:]))
+        if ptr3 != len(number) - 1:
+            ptr3 += 1
+        elif ptr2 != len(number) - 2:
+            ptr2 += 1
+        elif ptr1 != len(number) - 3:
+            ptr1 += 1
+        else:
+            break

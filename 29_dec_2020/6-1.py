54db2cc82ab9053eb795e281ef925036ea43b2dc
ironmanvim <vishalchiluveri@gmail.com>
2020-12-30 01:17:43 +0530

    added a method for the solution with test cases

--- 29_dec_2020/6-1.py
+++ 29_dec_2020/6-1.py
@@ -6,26 +6,34 @@ def number_with_leading_zeros(number):
     return str(int(number)) != number
 
 
-number = str(127001)
-ptr1, ptr2, ptr3 = 1, 2, 3
+def ip_addresses(number):
+    results = []
+    ptr1, ptr2, ptr3 = 1, 2, 3
 
-while True:
-    if not in_IP_range(int(number[:ptr1])) or number_with_leading_zeros(number[:ptr1]):
-        break
-    elif not in_IP_range(int(number[ptr1:ptr2])) or number_with_leading_zeros(number[ptr1:ptr2]):
-        ptr1 += 1
-    elif not in_IP_range(int(number[ptr2:ptr3])) or number_with_leading_zeros(number[ptr2:ptr3]):
-        ptr2 += 1
-    elif not in_IP_range(int(number[ptr3:])) or number_with_leading_zeros(number[ptr3:]):
-        ptr3 += 1
-    else:
-        print('{}.{}.{}.{}'.format(
-            number[0:ptr1], number[ptr1:ptr2], number[ptr2:ptr3], number[ptr3:]))
-        if ptr3 != len(number) - 1:
-            ptr3 += 1
-        elif ptr2 != len(number) - 2:
-            ptr2 += 1
-        elif ptr1 != len(number) - 3:
-            ptr1 += 1
-        else:
+    while True:
+        if not in_IP_range(int(number[:ptr1])) or number_with_leading_zeros(number[:ptr1]):
             break
+        elif not in_IP_range(int(number[ptr1:ptr2])) or number_with_leading_zeros(number[ptr1:ptr2]):
+            ptr1 += 1
+        elif not in_IP_range(int(number[ptr2:ptr3])) or number_with_leading_zeros(number[ptr2:ptr3]):
+            ptr2 += 1
+        elif not in_IP_range(int(number[ptr3:])) or number_with_leading_zeros(number[ptr3:]):
+            ptr3 += 1
+        else:
+            results.append('{}.{}.{}.{}'.format(
+                number[0:ptr1], number[ptr1:ptr2], number[ptr2:ptr3], number[ptr3:]))
+            if ptr3 != len(number) - 1:
+                ptr3 += 1
+            elif ptr2 != len(number) - 2:
+                ptr2 += 1
+            elif ptr1 != len(number) - 3:
+                ptr1 += 1
+            else:
+                break
+
+    return results
+
+
+print(ip_addresses('1592551013'))
+print(ip_addresses('10000'))
+print(ip_addresses('127001'))
